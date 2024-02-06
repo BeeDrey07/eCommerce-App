2f07d139a088b3e878eedf26320d5b9b4aa6a48b
beedrey07 <beedrey07@gmail.com>
2024-02-06 15:59:55 +0200

    Add To Cart Function

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -15,9 +15,7 @@ const cartSlice = createSlice({
     addToCart: (state, action) => {
       const item = action.payload;
 
-      const existItem = state.cartItems.find((x) => {
-        x._id === item._id;
-      });
+      const existItem = state.cartItems.find((x) => x._id === item._id);
 
       if (existItem) {
         state.cartItems = state.cartItems.map((x) =>
@@ -52,6 +50,6 @@ const cartSlice = createSlice({
   },
 });
 
-export const {addToCart} = cartSlice.actions;
+export const { addToCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
