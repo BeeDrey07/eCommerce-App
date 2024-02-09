c799c5b984addfeea6a5a9baff306222527b2eaa
beedrey07 <beedrey07@gmail.com>
2024-02-09 11:21:00 +0200

    Auth Middleware and Endpoint

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,21 +1,22 @@
 const notFound = (req, res, next) => {
-  const error = new Error("Not Found - `${req.originalUrl}`");
+  const error = new Error(`Not Found - ${req.originalUrl}`);
   res.status(404);
   next(error);
 };
 
 const errorHandler = (err, req, res, next) => {
-  let statusCode = res.statusCode === 200 ? 500 : res.status;
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   let message = err.message;
 
-  // Check for mongoose bad ObjectId
-  if (err.name === "CastError" && err.kind === "ObjectId");
-  message = "Resource Not Found";
-  statusCode = 404;
+  // If Mongoose not found error, set to 404 and change message
+  if (err.name === 'CastError' && err.kind === 'ObjectId') {
+    statusCode = 404;
+    message = 'Resource not found';
+  }
 
   res.status(statusCode).json({
-    message,
-    stack: process.env.NODE_ENV === "production" ? "pancake stack" : err.stack,
+    message: message,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
   });
 };
 
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -28,7 +28,6 @@ const HomeScreen = () => {
           </Row>
         </>
       )}
-      ;
     </>
   );
 };
