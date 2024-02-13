f45bfa05a028b6002d3ce8024594a3376ce3051e
beedrey07 <beedrey07@gmail.com>
2024-02-13 10:41:07 +0200

    Order API Slice and Start Order

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -16,6 +16,7 @@ import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ShippingScreen from "./screens/ShippingScreen";
 import PaymentScreen from "./screens/PaymentScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 
 const root = document.getElementById("root");
 
@@ -34,6 +35,8 @@ ReactDOM.render(
             <Route path="" element={<PrivateRoute />}>
               <Route path="/shipping" element={<ShippingScreen />} />
               <Route path="/payment" element={<PaymentScreen />} />
+              <Route path="/placeorder" element={<PlaceOrderScreen />} />
+
               <Route />
             </Route>
           </Route>
